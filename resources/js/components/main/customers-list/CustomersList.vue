08443c871e10f40f71eacc4fa5b3f54f62994973
hoseinpour <mh4620@gmail.com>
2019-07-23 11:09:39 +0430

    add customer list

--- resources/js/components/main/customers-list/CustomersList.vue
+++ resources/js/components/main/customers-list/CustomersList.vue
@@ -1,12 +1,35 @@
 <template>
-    <p>
-        مشتری ها
-    </p>
+    <div id="customer-list">
+        <h3>لیست مشتریان</h3>
+        <div>
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <th>ردیف</th>
+                    <th>نام</th>
+                    <th>شهر</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="(customer, index) in customers">
+                    <td>{{index + 1}}</td>
+                    <td>{{customer.name}}</td>
+                    <td>{{customer.city}}</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "CustomersList"
+        name: "CustomersList",
+        data(){
+            return {
+                customers:[]
+            }
+        },
     }
 </script>
 
