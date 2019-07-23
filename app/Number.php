e4b0c6467832dae6cd3fb299b160b0e48fe2a3e9
hoseinpour <mh4620@gmail.com>
2019-07-23 13:04:08 +0430

    add numbers table and model

---
+++ app/Number.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * @property mixed is_active
+ * @property mixed charge_type_id
+ * @property mixed phone_number_type_id
+ * @property mixed customer_id
+ * @property mixed phone_number
+ */
+class Number extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = ['phone_number_type_id','phone_number','charge_type_id','is_active'];
+
+    public function setNumberAttribute($value)
+    {
+        if($value == 'true'){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+}
---
+++ database/migrations/2019_07_23_070730_create_numbers_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNumbersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('numbers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('customer_id');
+            $table->foreign('customer_id')->references('id')->on('customers');
+            $table->unsignedBigInteger('phone_number_type_id');
+            $table->foreign('phone_number_type_id')->references('id')->on('phone_number_types');
+            $table->char('phone_number',20);
+            $table->unsignedBigInteger('charge_type_id');
+            $table->foreign('charge_type_id')->references('id')->on('charge_types');
+            $table->tinyInteger('is_active')->default(1);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('numbers');
+    }
+}
