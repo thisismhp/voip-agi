c6227ab0396037316a33d825e1042628c4cae578
hoseinpour <mh4620@gmail.com>
2019-07-23 11:09:52 +0430

    add demo users list

--- resources/js/components/main/demo-users-list/DemoUsersList.vue
+++ resources/js/components/main/demo-users-list/DemoUsersList.vue
@@ -1,10 +1,33 @@
 <template>
-    <p>لیست کاربران دمو</p>
+    <div id="demo-users-list">
+        <h3>لیست کاربران دمو</h3>
+        <div>
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <th>ردیف</th>
+                    <th>شماره</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="(demo_user, index) in demo_users">
+                    <td>{{index + 1}}</td>
+                    <td>{{demo_user.number}}</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "DemoUsersList"
+        name: "DemoUsersList",
+        data(){
+            return {
+                demo_users:[]
+            }
+        }
     }
 </script>
 
