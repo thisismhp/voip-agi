46926a80e28e3b50b107863cad119cb58d3d9ce6
Mohammad <mh4620@gmail.com>
2019-07-26 01:05:06 +0430

    add loading and reload functions

--- resources/js/components/main/customers-list/CustomersList.vue
+++ resources/js/components/main/customers-list/CustomersList.vue
@@ -1,44 +1,68 @@
 <template>
-    <div id="customer-list">
-        <h3>لیست مشتریان</h3>
-        <div>
-            <table class="table table-striped">
-                <thead>
-                <tr>
-                    <th>ردیف</th>
-                    <th>نام</th>
-                    <th>شهر</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr v-for="(customer, index) in customers">
-                    <td>{{index + 1}}</td>
-                    <td><router-link :to="'/customer/'+ customer.id">{{customer.name}}</router-link></td>
-                    <td>{{customer.city.name}}</td>
-                </tr>
-                </tbody>
-            </table>
+    <div>
+        <Loading v-if="loading" />
+        <div class="failed center-align" v-else-if="loadFailed">
+            <button @click="reload" class="btn btn-warning">تلاش مجدد</button>
+        </div>
+        <div v-else-if="!loading" id="customer-list">
+            <h3>لیست مشتریان</h3>
+            <div>
+                <table class="table table-striped">
+                    <thead>
+                    <tr>
+                        <th>ردیف</th>
+                        <th>نام</th>
+                        <th>شهر</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr v-for="(customer, index) in customers">
+                        <td>{{index + 1}}</td>
+                        <td><router-link :to="'/customer/'+ customer.id">{{customer.name}}</router-link></td>
+                        <td>{{customer.city.name}}</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
     import axios from 'axios'
+    import Loading from "../../layout/element/Loading";
     export default {
         name: "CustomersList",
+        components: {Loading},
         data(){
             return {
+                loading: true,
+                loadFailed:false,
                 customers:[]
             }
         },
         methods:{
+            err(err){
+                this.loadFailed = true;
+                this.loading = false;
+                return err;
+            },
+            reload(){
+                this.loading = true;
+                this.loadFailed = false;
+                this.getCustomers();
+            },
             getCustomers(){
                 axios.get('api/customer')
                     .then(res => {
                         this.customers = res.data;
+                        this.loading = false;
+
                     })
                     .catch(err => {
+                        this.err(err)
                     })
+                ;
             }
         },
         created() {
--- resources/js/components/main/demo-users-list/DemoUsersList.vue
+++ resources/js/components/main/demo-users-list/DemoUsersList.vue
@@ -1,44 +1,66 @@
 <template>
-    <div id="demo-users-list">
-        <h3>لیست کاربران دمو</h3>
-        <div>
-            <table class="table table-striped">
-                <thead>
-                <tr>
-                    <th>ردیف</th>
-                    <th>شماره</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr v-for="(demoUser, index) in demoUsers">
-                    <td>{{index + 1}}</td>
-                    <td>{{demoUser.phone_number}}</td>
-                </tr>
-                </tbody>
-            </table>
+    <Loading v-if="loading" />
+    <div class="failed center-align" v-else-if="loadFailed">
+        <button @click="reload" class="btn btn-warning">تلاش مجدد</button>
+    </div>
+    <div v-else-if="!loading">
+        <div id="demo-users-list">
+            <h3>لیست کاربران دمو</h3>
+            <div>
+                <table class="table table-striped">
+                    <thead>
+                    <tr>
+                        <th>ردیف</th>
+                        <th>شماره</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr v-for="(demoUser, index) in demoUsers">
+                        <td>{{index + 1}}</td>
+                        <td>{{demoUser.phone_number}}</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
     import axios from "axios";
+    import Loading from "../../layout/element/Loading";
 
     export default {
         name: "DemoUsersList",
+        components: {Loading},
         data(){
             return {
+                loading: true,
+                loadFailed:false,
                 demoUsers:[]
             }
         },
         methods:{
+            err(err){
+                this.loadFailed = true;
+                this.loading = false;
+                return err;
+            },
+            reload(){
+                this.loading = true;
+                this.loadFailed = false;
+                this.getDemoUsers();
+            },
             getDemoUsers(){
                 axios.get('api/demo_user')
                     .then(res => {
                         this.demoUsers = res.data;
+                        this.loading = false;
                     })
                     .catch(err => {
-                        console.log(err);
+                        this.err(err)
                     })
+                ;
             }
         },
         created() {
