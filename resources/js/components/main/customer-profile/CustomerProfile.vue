d097358e78cd85446881ab96ad81d1b76a9e0a99
Mohammad <mh4620@gmail.com>
2019-07-26 22:58:18 +0430

    add response dialog

--- resources/js/components/main/customer-profile/CustomerProfile.vue
+++ resources/js/components/main/customer-profile/CustomerProfile.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <DialogMessage :show="dialogVars.show" :title="dialogVars.title" :content="dialogVars.content" :mode="dialogVars.mode" :show-time="dialogVars.showTime" @show="dialogVars.show = false"/>
         <Loading v-if="loading" />
         <div class="failed center-align" v-else-if="loadFailed">
             <button @click="reload" class="btn btn-warning">تلاش مجدد</button>
@@ -112,9 +113,12 @@
     import axios from 'axios';
     import VuePersianDatetimePicker from 'vue-persian-datetime-picker';
     import Loading from "../../layout/element/Loading";
+    import DialogMessage from "../../layout/element/DialogMessage";
+    import {mixins} from "../../../mixins";
     export default {
         name: "CustomerProfile",
         components: {
+            DialogMessage,
             Loading,
             datePicker: VuePersianDatetimePicker
         },
@@ -123,6 +127,14 @@
                 sending: false,
                 loading: true,
                 loadFailed:false,
+                dialogVars:{
+                    title:'',
+                    content:'',
+                    mode:'',
+                    show:false,
+                    showTime:2000
+
+                },
                 provincesIsLoad: false,
                 id: null,
                 customerData:[],
@@ -142,6 +154,13 @@
                 this.loading = true;
                 this.loadFailed = false;
                 this.load(this.id);
+            },
+            showDialog(show, title, content, mode, showTime){
+                this.dialogVars.show=show;
+                this.dialogVars.title=title;
+                this.dialogVars.content=content;
+                this.dialogVars.mode=mode;
+                this.dialogVars.showTime=showTime;
             },
             initForm() {
                 axios.get('/api/province')
@@ -199,15 +218,28 @@
                     })
                 ;
             },
+            errorHandling(err){
+                if(err.response){
+                    if(err.response.status === 422){
+                        this.showDialog(true, "ثبت ناموفق",mixins.parseValidation(err.response),'danger',0);
+                    }else{
+                        this.showDialog(true, "خطای سرور",'مشکلی در سرور به وجود آمده است','danger',0);
+                    }
+                }else {
+                    this.showDialog(true, "خطای ارتباط با سرور","ارتباط با سرور انجام نشد",'danger',0);
+                }
+            },
             update(id){
                 this.sending = true;
                 axios.patch(`/api/customer/${id}`,this.customerData)
                     .then(res => {
                         this.customerData = res.data;
                         this.sending = false;
+                        this.showDialog(true, "ثبت موفق","اطلاعات با موفقیت ثبت شد.",'success',2000);
                     })
                     .catch(err => {
                         this.sending = false;
+                        this.errorHandling(err);
                     })
                 ;
             },
