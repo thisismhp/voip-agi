dab111522518922e76cb29267ba7e46437c32972
Mohammad <mh4620@gmail.com>
2019-07-26 23:59:46 +0430

    Init not found component

---
+++ resources/js/components/layout/element/NotFound.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1 class="center-align not-found">
+            {{notFoundTitle}}
+            مورد نظر شما وجود ندارد.
+        </h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "NotFound",
+        props:{
+            notFoundTitle:{
+                required:false,
+                default:'صفحه',
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
