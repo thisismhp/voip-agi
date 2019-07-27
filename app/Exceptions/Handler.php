0e57913e73d3bb9fd7e36c35305194920963122a
Mohammad <mh4620@gmail.com>
2019-07-27 23:51:21 +0430

    convert 405 error to 404

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -35,6 +36,9 @@ class Handler extends ExceptionHandler
      */
     public function report(Exception $exception)
     {
+        if($exception instanceof MethodNotAllowedHttpException){
+            abort(404);
+        }
         parent::report($exception);
     }
 
