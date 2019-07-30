505ce6e6adc267be04454ef6e6202db81bc4ac5b
Mohammad <mh4620@gmail.com>
2019-07-30 21:01:25 +0430

    fix route Access

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::namespace('Api')->group(function () {
-    Route::get('province','ProvinceController');
-    Route::get('city','CityController');
-    Route::get('phone_number_type','PhoneNumberTypeController');
-    Route::get('charge_type','ChargeTypeController');
-    Route::apiResource('customer','CustomerController');
-    Route::get('demo_user','DemoUserController');
-    Route::post('charge','ChargeController');
-});
+if (request()->expectsJson()) {
+    Route::namespace('Api')->group(function () {
+        Route::get('province', 'ProvinceController');
+        Route::get('city', 'CityController');
+        Route::get('phone_number_type', 'PhoneNumberTypeController');
+        Route::get('charge_type', 'ChargeTypeController');
+        Route::apiResource('customer', 'CustomerController');
+        Route::get('demo_user', 'DemoUserController');
+        Route::post('charge', 'ChargeController');
+    });
+}
