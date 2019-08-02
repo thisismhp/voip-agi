b588b008a38c05c3ff3894dd5f27fc91e62efee7
Mohammad <mh4620@gmail.com>
2019-08-02 23:59:28 +0430

    init component template

--- resources/js/components/main/charge-multi-demo-user/ChargeMultiDemoUser.vue
+++ resources/js/components/main/charge-multi-demo-user/ChargeMultiDemoUser.vue
@@ -1,10 +1,76 @@
 <template>
-    <p>افزایش اعتبار گروهی دمو</p>
+    <div>
+        <DialogMessage :show="dialogVars.show" :title="dialogVars.title" :content="dialogVars.content" :mode="dialogVars.mode" :show-time="dialogVars.showTime" @show="dialogVars.show = false"/>
+        <Loading v-if="loading" />
+        <div class="failed center-align" v-else-if="loadFailed">
+            <button @click="reload" class="btn btn-warning">تلاش مجدد</button>
+        </div>
+        <div v-else-if="!loading" id="cmd-form">
+            <h3 class="border-bottom">افزایش اعتبار گروهی کاربران دمو</h3>
+            <div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+    import DialogMessage from "../../layout/element/DialogMessage";
+    import Loading from "../../layout/element/Loading";
+    import {mixins} from "../../../mixins";
     export default {
-        name: "ChargeMultiDemoUser"
+        name: "ChargeMultiDemoUser",
+        components: {Loading, DialogMessage},
+        data(){
+            return {
+                sending: false,
+                loading: true,
+                loadFailed:false,
+                dialogVars:{
+                    title:'',
+                    content:'',
+                    mode:'',
+                    show:false,
+                    showTime:2000
+
+                },
+            }
+        },
+        methods:{
+            err(err){
+                this.loadFailed = true;
+                this.loading = false;
+                return err;
+            },
+            reload(){
+                this.loading = true;
+                this.loadFailed = false;
+                this.initForm();
+            },
+            showDialog(show, title, content, mode, showTime){
+                this.dialogVars.show=show;
+                this.dialogVars.title=title;
+                this.dialogVars.content=content;
+                this.dialogVars.mode=mode;
+                this.dialogVars.showTime=showTime;
+            },
+            initForm() {
+                this.loading = false;
+            },
+            errorHandling(err){
+                if(err.response){
+                    if(err.response.status === 422){
+                        this.showDialog(true, "ثبت ناموفق",mixins.parseValidation(err.response),'danger',0);
+                    }else{
+                        this.showDialog(true, "خطای سرور",'مشکلی در سرور به وجود آمده است','danger',0);
+                    }
+                }else {
+                    this.showDialog(true, "خطای ارتباط با سرور","ارتباط با سرور انجام نشد",'danger',0);
+                }
+            },
+        },
+        created() {
+            this.initForm();
+        }
     }
 </script>
 
