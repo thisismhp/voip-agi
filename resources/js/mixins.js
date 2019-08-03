4246fdfced47fc2d90901da919c16dcfe756738f
Mohammad <mh4620@gmail.com>
2019-08-03 23:18:42 +0430

    add isNumber function

--- resources/js/mixins.js
+++ resources/js/mixins.js
@@ -22,6 +22,14 @@ export const mixins = {
         else{
             return 'ورودی ها اشتباه است';
         }
+    },
+    isNumber : function(evt) {
+        const charCode = (evt.which) ? evt.which : evt.keyCode;
+        if ((charCode > 31 && (charCode < 48 || charCode > 57))) {
+            evt.preventDefault();
+        } else {
+            return true;
+        }
     }
 };
 
