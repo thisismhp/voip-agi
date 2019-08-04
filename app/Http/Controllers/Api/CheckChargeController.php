0f4262d5c5e07209713db53c1d6220d69ff86faa
Mohammad <mh4620@gmail.com>
2019-08-05 00:25:40 +0430

    init

---
+++ app/Http/Controllers/Api/CheckChargeController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Customer;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Response;
+use Illuminate\Validation\Rule;
+
+class CheckChargeController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function __invoke(Request $request)
+    {
+        $request->validate([
+           'customer_id' => ['required',Rule::exists('customers','id')->whereNull('deleted_at')]
+        ]);
+        $customer = Customer::find($request->input('customer_id'));
+        if($customer->end_charge_checked == 2){
+            $customer->update(['end_charge_checked' => 3]);
+        }
+        return new Response($customer);
+    }
+}
