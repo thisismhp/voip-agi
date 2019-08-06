7691749bd2770fa3fb9cc7b60eee443c00099bf5
Mohammad <mh4620@gmail.com>
2019-08-06 13:01:09 +0430

    add vuex

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,13 @@
 import VueRouter from 'vue-router'
+import Vuex from 'vuex'
 import App from './components/App'
 import {routes} from "./routes";
+import {store} from "./store";
 
 require('./bootstrap');
 window.Vue = require('vue');
 Vue.use(VueRouter);
+Vue.use(Vuex);
 
 const router = new VueRouter({
     mode: 'history',
@@ -16,4 +19,5 @@ new Vue({
     components: { App },
     render: h => h(App),
     router,
+    store
 });
