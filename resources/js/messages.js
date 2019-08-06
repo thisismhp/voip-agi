2940156135e07ff1d7a1dd4f0a2820648a345158
Mohammad <mh4620@gmail.com>
2019-08-06 18:38:26 +0430

    init

---
+++ resources/js/messages.js
@@ -0,0 +1,26 @@
+export const messages = {
+    fa: {
+        words:{
+            dashboard:'داشبورد',
+            logout:'خروج',
+            customersManage:'مدیریت مشتریان',
+            addCustomer:'افزودن مشتری جدید',
+            customersList:'لیست مشتریان',
+            demoUsersList:'لیست کاربران دمو',
+            ioc:'افزایش اعتبار انفرادی مشتریان',
+            iod:'افزایش اعتبار انفرادی دمو',
+            imc:'فزایش اعتبار گروهی مشتریان',
+            imd:'افزایش اعتبار گروهی دمو',
+            cNoChargeList:'لیست اتمام اعتبار ( بررسی شده)',
+            ucNoChargeList:'لیست اتمام اعتبار ( بررسی نشده)',
+            servicesManage:'مدیریت سرویس ها',
+            addService:'افزودن سرویس جدید',
+            servicesList:'لیست سرویس ها',
+            addServiceGroup:'افزودن گروه های سرویس',
+            symbolsList:'لیست نمادها',
+        },
+        messages:{
+
+        }
+    },
+};
