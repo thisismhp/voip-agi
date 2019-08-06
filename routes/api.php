695de287356be135cc8b88aa105f919d18f2b200
Mohammad <mh4620@gmail.com>
2019-08-06 10:34:28 +0430

    fix a bug

--- routes/api.php
+++ routes/api.php
@@ -3,16 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 if (request()->expectsJson()) {
-    Route::namespace('Api')->group(function () {
-        Route::apiResource('customer', 'CustomerController');
-        Route::post('charge', 'ChargeController');
-        Route::post('check_charge', 'CheckChargeController');
-        Route::post('cmt', 'CMTController');
-        Route::get('province', 'ProvinceController');
-        Route::get('city', 'CityController');
-        Route::get('phone_number_type', 'PhoneNumberTypeController');
-        Route::get('charge_type', 'ChargeTypeController');
-        Route::get('demo_user', 'DemoUserController');
-        Route::get('no_charge', 'NoChargeController');
-    });
+    if(auth()->check()) {
+        Route::namespace('Api')->group(function () {
+            Route::apiResource('customer', 'CustomerController');
+            Route::post('charge', 'ChargeController');
+            Route::post('check_charge', 'CheckChargeController');
+            Route::post('cmt', 'CMTController');
+            Route::get('province', 'ProvinceController');
+            Route::get('city', 'CityController');
+            Route::get('phone_number_type', 'PhoneNumberTypeController');
+            Route::get('charge_type', 'ChargeTypeController');
+            Route::get('demo_user', 'DemoUserController');
+            Route::get('no_charge', 'NoChargeController');
+        });
+    }
+    else{
+        abort(401);
+    }
 }
