8ed5eadfbea2a2e82f6680cf8d131130a45a9418
Mohammad <mh4620@gmail.com>
2019-08-07 23:05:58 +0430

    change response

--- app/Http/Controllers/Api/CheckAccessController.php
+++ app/Http/Controllers/Api/CheckAccessController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Api;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Response;
 
@@ -11,11 +10,10 @@ class CheckAccessController extends Controller
     /**
      * Handle the incoming request.
      *
-     * @param Request $request
      * @return Response
      */
-    public function __invoke(Request $request)
+    public function __invoke()
     {
-        return new Response($request);
+        return new Response(auth()->user());
     }
 }
