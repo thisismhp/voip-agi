d865f95222c3a9ceae9daa327f1b54aa6b7eb0d2
Mohammad <mh4620@gmail.com>
2019-08-07 17:58:44 +0430

    add header

--- resources/js/components/layout/content/Content.vue
+++ resources/js/components/layout/content/Content.vue
@@ -1,14 +1,16 @@
 <template>
-    <div id="content" class="content">
+    <div id="content" class="content container-fluid">
+        <Header />
         <Main />
     </div>
 </template>
 
 <script>
     import Main from "../../main/Main";
+    import Header from "../header/Header";
     export default {
         name: "Content",
-        components: {Main}
+        components: {Header, Main},
     }
 </script>
 
