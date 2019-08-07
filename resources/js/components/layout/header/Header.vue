919e949e26a638f79c4b49a7b95f91a546a82ce8
Mohammad <mh4620@gmail.com>
2019-08-07 17:58:03 +0430

    init

---
+++ resources/js/components/layout/header/Header.vue
@@ -0,0 +1,45 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <button type="button" @click="showSide" class="btn btn-info">
+                <img class="menu-icon" src="../../../../icon/menu.png"  alt="Menu" />
+            </button>
+            <button @click="logout" style="margin:5px" class="btn btn-secondary" :disabled="sending">{{$t("words.logout")}}
+                <span v-if="sending" class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+            </button>
+        </div>
+    </nav>
+</template>
+
+<script>
+    import {mixins} from "../../../mixins";
+    import axios from "axios";
+    export default {
+        name: "Header",
+        data(){
+            return{
+                sending: false,
+            }
+        },
+        methods:{
+            showSide : mixins.showSide,
+            logout(){
+                this.sending = true;
+                axios.patch('/api/logout')
+                    .then(() => {
+                        this.$store.state.authCheck = false;
+                        window.axios.defaults.headers.common['Authorization'] = null;
+                        this.sending = false;
+                    })
+                    .catch(() => {
+                        this.sending = false;
+                    })
+                ;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
