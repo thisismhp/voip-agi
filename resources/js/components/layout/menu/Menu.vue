b3a342695b3fcf0aa39325d776db75585c42ad38
Mohammad <mh4620@gmail.com>
2019-08-08 11:24:24 +0430

    hide menu on click in mobile view

--- resources/js/components/layout/menu/Menu.vue
+++ resources/js/components/layout/menu/Menu.vue
@@ -2,29 +2,29 @@
     <div id="menu">
         <nav id="sidebar" class="sidebar active">
             <div class="tiny-margin d-block d-sm-none d-none d-sm-block d-md-none">
-                <button type="button" @click="showSide" class="btn btn-secondary">
+                <button type="button" @click="hideSide" class="btn btn-secondary">
                     <img class="menu-icon" src="../../../../image/menu.png" alt="Menu" />
                 </button>
             </div>
             <div id="customer-mng-links">
-                <router-link to="/" active-class="active" exact>{{$t("words.dashboard")}}</router-link>
+                <router-link @click.native="menuSlideHide" to="/" active-class="active" exact>{{$t("words.dashboard")}}</router-link>
                 <div class="menu-title">{{$t("words.customersManage")}}</div>
-                <router-link to="/add-customer" active-class="active">{{$t("words.addCustomer")}}</router-link>
-                <router-link to="/customer-list" active-class="active">{{$t("words.customersList")}}</router-link>
-                <router-link to="/demo-users-list" active-class="active">{{$t("words.demoUsersList")}}</router-link>
-                <router-link to="/charge-one-customer" active-class="active">{{$t("words.ioc")}}</router-link>
-                <router-link to="/charge-one-demo-user" active-class="active">{{$t("words.iod")}}</router-link>
-                <router-link to="/charge-multi-customer" active-class="active">{{$t("words.imc")}}</router-link>
-                <router-link to="/charge-multi-demo-user" active-class="active">{{$t("words.imd")}}</router-link>
-                <router-link to="/no-charge-checked-list" active-class="active">{{$t("words.cNoChargeList")}}</router-link>
-                <router-link to="/no-charge-unchecked-list" active-class="active">{{$t("words.ucNoChargeList")}}</router-link>
+                <router-link @click.native="menuSlideHide" to="/add-customer" active-class="active">{{$t("words.addCustomer")}}</router-link>
+                <router-link @click.native="menuSlideHide" to="/customer-list" active-class="active">{{$t("words.customersList")}}</router-link>
+                <router-link @click.native="menuSlideHide" to="/demo-users-list" active-class="active">{{$t("words.demoUsersList")}}</router-link>
+                <router-link @click.native="menuSlideHide" to="/charge-one-customer" active-class="active">{{$t("words.ioc")}}</router-link>
+                <router-link @click.native="menuSlideHide" to="/charge-one-demo-user" active-class="active">{{$t("words.iod")}}</router-link>
+                <router-link @click.native="menuSlideHide" to="/charge-multi-customer" active-class="active">{{$t("words.imc")}}</router-link>
+                <router-link @click.native="menuSlideHide" to="/charge-multi-demo-user" active-class="active">{{$t("words.imd")}}</router-link>
+                <router-link @click.native="menuSlideHide" to="/no-charge-checked-list" active-class="active">{{$t("words.cNoChargeList")}}</router-link>
+                <router-link @click.native="menuSlideHide" to="/no-charge-unchecked-list" active-class="active">{{$t("words.ucNoChargeList")}}</router-link>
             </div>
             <div id="customer-serv-links">
                 <div class="menu-title">{{$t("words.servicesManage")}}</div>
-                <router-link to="/add-service" active-class="active" exact>{{$t("words.addService")}}</router-link>
-                <router-link to="/services-list" active-class="active">{{$t("words.servicesList")}}</router-link>
-                <router-link to="/add-service-groups" active-class="active">{{$t("words.addServiceGroup")}}</router-link>
-                <router-link to="/symbols-list" active-class="active">{{$t("words.symbolsList")}}</router-link>
+                <router-link @click.native="menuSlideHide" to="/add-service" active-class="active" exact>{{$t("words.addService")}}</router-link>
+                <router-link @click.native="menuSlideHide" to="/services-list" active-class="active">{{$t("words.servicesList")}}</router-link>
+                <router-link @click.native="menuSlideHide" to="/add-service-groups" active-class="active">{{$t("words.addServiceGroup")}}</router-link>
+                <router-link @click.native="menuSlideHide" to="/symbols-list" active-class="active">{{$t("words.symbolsList")}}</router-link>
             </div>
         </nav>
     </div>
@@ -36,7 +36,13 @@
     export default {
         name: "Menu",
         methods:{
-            showSide : mixins.showSide,
+            hideSide : mixins.hideSide,
+            menuSlideHide () {
+                console.log(window.innerWidth);
+                if(window.innerWidth < 768){
+                    this.hideSide();
+                }
+            }
         }
     }
 </script>
