6f667285f1b6df8878a5344df4602152165d0545
Mohammad <mh4620@gmail.com>
2019-08-10 18:36:04 +0430

    add is soft delete

--- app/Service.php
+++ app/Service.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Service extends Model
 {
+    use SoftDeletes;
+
     protected $connection = "manage";
 
     protected $fillable = ['name','m_line','w_line','ws_username','ws_username','ws_password','ws_update_interval','user_id'];
