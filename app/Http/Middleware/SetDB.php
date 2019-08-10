2ffc5e87cf7dcc7e2a5585c64d3f3a650f4e20b3
Mohammad <mh4620@gmail.com>
2019-08-10 13:16:22 +0430

    add admin access

--- app/Http/Middleware/SetDB.php
+++ app/Http/Middleware/SetDB.php
@@ -3,14 +3,15 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\Request;
 
 class SetDB
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param Request  $request
+     * @param Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
@@ -18,15 +19,20 @@ class SetDB
         $serviceID = $request->header('serviceid');
         if ($this->user()->id == 1 and $this->user()->username == 'admin'){
             config(['database.connections.service.database' => "service-$serviceID"]);
+            return $next($request);
         }else{
             $services = $this->user()->services;
             foreach ($services as $service) {
                 if ($service->id == $serviceID) {
                     config(['database.connections.service.database' => "service-$serviceID"]);
+                    return $next($request);
                 }
             }
         }
-        return $next($request);
+        if(substr($request->getRequestUri(),0,12) != "/api/access/"){
+            return $next($request);
+        }
+        return abort(403);
     }
 
     private function user()
