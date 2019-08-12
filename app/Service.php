b4ef6116bc4e42d85f856be394fd0ee4132ccc8b
Mohammad <m.ho3einpour@gmail.com>
2019-08-12 23:25:17 +0430

    add user id property

--- app/Service.php
+++ app/Service.php
@@ -5,6 +5,9 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * @property mixed user_id
+ */
 class Service extends Model
 {
     use SoftDeletes;
