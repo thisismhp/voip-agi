dccb14ba50de33c52d33eea8e82aa7dcd863e707
Mohammad <m.ho3einpour@gmail.com>
2019-08-12 20:51:40 +0430

    add admin user

---
+++ database/migrations/2017_01_01_000000_create_users_table.php
@@ -0,0 +1,65 @@
+<?php
+
+use App\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('username')->unique();
+            $table->string('name');
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+        $admin = new User();
+        $admin->username = 'admin';
+        $admin->name = 'admin';
+        $admin->password = bcrypt('12345678');
+        $admin->save();
+        $now = now();
+        DB::unprepared("INSERT INTO `oauth_clients`
+            (
+                `name`,
+                `secret`,
+                `redirect`,
+                `personal_access_client`,
+                `password_client`,
+                `revoked`,
+                `created_at`,
+                `updated_at`
+            )
+            VALUES(
+                'vue client',
+                'voip.vue.secret',
+                'http://localhost',
+                '0',
+                '1',
+                '0',
+                '$now',
+                '$now'
+            )
+        ");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
