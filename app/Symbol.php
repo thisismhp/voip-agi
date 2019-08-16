e41d754980f3c1a6e01a6e395290d727c2db727a
Mohammad <m.ho3einpour@gmail.com>
2019-08-16 13:28:05 +0430

    add unit

--- app/Symbol.php
+++ app/Symbol.php
@@ -9,4 +9,11 @@ class Symbol extends Model
     protected $connection = "service";
 
     protected $fillable = ['symbolId','is_active','m_file','w_file','unit_id','fName','eName','buyPriceFormatted','sellPriceFormatted','transPriceFormatted','buysellDiff','vol','volTick','direction','transType','dsabt'];
+
+    protected $with = ['unit'];
+
+    public function unit()
+    {
+        return $this->belongsTo(Unit::class);
+    }
 }
