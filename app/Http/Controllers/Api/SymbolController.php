b4c19064c69ccd04d26f6774597f486e6bcf3605
Mohammad <m.ho3einpour@gmail.com>
2019-08-18 21:51:05 +0430

    modify update

--- app/Http/Controllers/Api/SymbolController.php
+++ app/Http/Controllers/Api/SymbolController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Service;
 use App\Symbol;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -41,7 +42,22 @@ class SymbolController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $request->validate([
+            'is_active' => 'required|boolean',
+            'unit_id' => 'required|exists:manage.units,id',
+            'm_file' => 'nullable|mimetypes:audio/mpeg',
+            'w_file' => 'nullable|mimetypes:audio/mpeg'
+        ]);
+        $path = "services/symbols/" . Service::currentService()->id;
         $symbol = Symbol::findOrFail($id);
+        if($request->file('m_file') != null){
+            $this->storeFiles($request, $path, ['m_file']);
+            $symbol->update(['m_file' => 1]);
+        }
+        if($request->file('w_file') != null){
+            $this->storeFiles($request, $path, ['w_file']);
+            $symbol->update(['w_file' => 1]);
+        }
         $symbol->update($request->only(['is_active','unit_id']));
         return new Response($symbol);
     }
