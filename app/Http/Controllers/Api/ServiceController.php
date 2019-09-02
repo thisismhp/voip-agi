79d525f29959304ff0503112404e9132dca44429
Mohammad <mh4620@gmail.com>
2019-09-02 12:05:59 +0430

    add fields

--- app/Http/Controllers/Api/ServiceController.php
+++ app/Http/Controllers/Api/ServiceController.php
@@ -44,6 +44,10 @@ class ServiceController extends Controller
         $service->m_line = $request->input('m_line');
         $service->w_line = $request->input('w_line');
         $service->is_active = $request->input('is_active');
+        $service->customer_is_free = $request->input('customer_is_free');
+        $service->demo_is_free = $request->input('demo_is_free');
+        $service->demo_first_charge = $request->input('demo_first_charge');
+        $service->demo_charge_type_id = $request->input('demo_charge_type_id');
         $service->ws_address = $request->input('ws_address');
         $service->ws_username = $request->input('ws_username');
         $service->ws_password = $request->input('ws_password');
@@ -94,7 +98,7 @@ class ServiceController extends Controller
             abort(403);
         }
         $this->validateRequest($request, true);
-        $service->update($request->only(['name','m_line','w_line','is_active','ws_address','ws_username','ws_password','ws_update_interval','user_id']));
+        $service->update($request->only(['name','m_line','w_line','is_active','customer_is_free','demo_is_free','demo_first_charge','demo_charge_type_id','ws_address','ws_username','ws_password','ws_update_interval','user_id']));
         DefaultSymbols::truncate();
         $dss = $request->input('default_symbols');
         foreach ((array)$dss as $ds) {
@@ -146,6 +150,10 @@ class ServiceController extends Controller
             'm_line' => ['required','string','max:50'],
             'w_line' => ['required','string','max:50'],
             'is_active' => ['required','boolean'],
+            'customer_is_free' => ['required','boolean'],
+            'demo_is_free' => ['required','boolean'],
+            'demo_first_charge' => ['required','integer','min:1'],
+            'demo_charge_type_id' => ['required','exists:manage.charge_types,id'],
             'ws_address' => ['required','string','max:250'],
             'ws_username' => ['required','string','max:250'],
             'ws_password' => ['required','string','max:250'],
