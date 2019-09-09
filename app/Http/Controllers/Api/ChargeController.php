2b82249dc1b4f62dbce5c4582a1dca10531edf11
Mohammad <m.ho3einpour@gmail.com>
2019-09-09 15:37:08 +0430

    change strategy

--- app/Http/Controllers/Api/ChargeController.php
+++ app/Http/Controllers/Api/ChargeController.php
@@ -21,75 +21,77 @@ class ChargeController extends Controller
      */
     public function __invoke(Request $request)
     {
-        $export = [];
         $destinationType = $request->input('destination_type');
-        $chargeTypeId = $request->input('charge_type_id');
-        $value = (int)$request->input('value');
+        $date_value = (int)$request->input('date_value');
+        $time_value = (int)$request->input('time_value');
+        $destinationID = $request->input('destination');
         $this->validateRequest($request, $destinationType);
-        $items = $request->input('items');
-        foreach ((array)$items as $item) {
-            $destination = null;
-            $chargeRecord = null;
-            $destinationId = $item;
-            if($destinationType == 1) {
-                $destination = Customer::find($destinationId);
-                $chargeRecord = new CustomerCharge();
-            }elseif ($destinationType == 2){
-                $destination = DemoUser::find($destinationId);
-                $chargeRecord = new DemoUserCharge();
+        $items = [];
+        $chargeRecord = null;
+        if ($destinationType == 1) {
+            $items[] = Customer::find($destinationID);
+        }
+        elseif ($destinationType == 2){
+            $items[] = DemoUser::find($destinationID);
+        }
+        elseif ($destinationType == 3) {
+            $items = Customer::all();
+        }
+        elseif ($destinationType == 4){
+            $items = DemoUser::all();
+        }
+        foreach ($items as $item) {
+            if($time_value > 0) {
+                $lastCharge = (int)$item->time_charge;
+                $newCharge = $lastCharge + $time_value;
+                $item->update(['time_charge' => $newCharge,'end_charge_checked' => 1,'end_charge_comment' => null]);
+                if($destinationType == 1 or $destinationType == 3){
+                    $chargeRecord = new CustomerCharge();
+                }else{
+                    $chargeRecord = new DemoUserCharge();
+                }
+                $chargeRecord->destination_id = $item->id;
+                $chargeRecord->value = $time_value;
+                $chargeRecord->charge_type_id = 1;
+                $chargeRecord->save();
             }
-            $chargeRecord->destination_id = $destinationId;
-            $chargeRecord->value = $value;
-            $chargeRecord->charge_type_id = $chargeTypeId;
-            $chargeRecord->save();
-            if($chargeTypeId == 1) {
-                $lastCharge = (int)$destination->time_charge;
-                $newCharge = $lastCharge + $value;
-                $destination->update(['time_charge' => $newCharge,'end_charge_checked' => 1,'end_charge_comment' => null]);
-            }
-            elseif ($chargeTypeId == 2){
-                $lastCharge = $destination->date_charge;
+            if ($date_value > 0){
+                if($destinationType == 1 or $destinationType == 3){
+                    $chargeRecord = new CustomerCharge();
+                }else{
+                    $chargeRecord = new DemoUserCharge();
+                }
+                $lastCharge = $item->date_charge;
                 $lastChargeDate = now();
                 if($lastCharge != null){
                     if(now() < $lastCharge){
                         $lastChargeDate = $lastCharge;
                     }
                 }
-                $newCharge = date("Y/m/d 23:59:59",strtotime("$lastChargeDate + $value day"));
-                $destination->update(['date_charge' => $newCharge,'end_charge_checked' => 1,'end_charge_comment' => null]);
+                $newCharge = date("Y/m/d 23:59:59",strtotime("$lastChargeDate + $date_value day"));
+                $item->update(['date_charge' => $newCharge,'end_charge_checked' => 1,'end_charge_comment' => null]);
+                $chargeRecord->destination_id = $item->id;
+                $chargeRecord->value = $date_value;
+                $chargeRecord->charge_type_id = 1;
+                $chargeRecord->save();
             }
-            array_push($export, $destination);
         }
-        return new Response($request);
+        return new Response(["message" => "OK"]);
     }
 
     public function validateRequest(Request $request, $destinationType)
     {
         $request->validate([
-            'destination_type' => ['required',Rule::in([1,2])],
-            'charge_type_id' => ['required',Rule::exists('manage.charge_types','id')->whereNot('id',3)],
-            'value' => ['required','integer','min:1'],
-            'items' => ['required','array','min:1']
+            'destination_type' => ['required',Rule::in([1,2,3,4])],
+            'date_value' => ['nullable','integer'],
+            'time_value' => ['nullable','integer'],
             ]);
-        $items = $request->input('items');
         $rules = [];
-        $messages = [
-            'items.required' => 'لیست افزایش اعتبار الزامی است',
-            'items.array' => 'لیست افزایش اعتبار اشتباه است',
-        ];
-        foreach ((array)$items as $i=>$item) {
             if($destinationType == 1){
-                $rules += ["items.$i" => [Rule::exists('service.customers', 'id')->whereNull('deleted_at')],];
-                $messages += [
-                    "items.$i.exists" => "فیلد مشتری ردیف " . ($i+1) . "معتبر نیست",
-                ];
+                $rules += ["destination" => ['required',Rule::exists('service.customers', 'id')->whereNull('deleted_at')],];
             }elseif($destinationType == 2){
-                $rules += ["items.$i" => [Rule::exists('service.demo_users', 'id')->whereNull('deleted_at')],];
-                $messages += [
-                    "items.$i.exists" => "فیلد کاربر دمو ردیف " . ($i+1) . "معتبر نیست",
-                ];
+                $rules += ["destination" => ['required',Rule::exists('service.demo_users', 'id')->whereNull('deleted_at')],];
             }
-        }
-        $request->validate($rules, $messages);
+        $request->validate($rules);
     }
 }
